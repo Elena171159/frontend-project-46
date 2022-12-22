8f8297ebbed1771692d578c132981a91ad68ce8c
Elena Diachkova <zabava1711@bk.ru>
2022-12-23 00:49:28 +0300

    add test yml and parse yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,9 +10,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 // console.log(getFixturePath('file1.json'));
-// const diff = makeDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
 const expected = readFileSync(getFixturePath('expectedjson.txt'), 'utf-8');
 
 test('flat Json', () => {
   expect(makeDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected);
+});
+test('flat yml', () => {
+  expect(makeDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toBe(expected);
+  expect(makeDiff(getFixturePath('file3.yaml'), getFixturePath('file2.yml'))).toBe(expected);
 });
