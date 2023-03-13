a0e841719565a77f5f0f57cf9591b0c4cb093935
Elena Diachkova <zabava1711@bk.ru>
2023-03-13 22:21:22 +0300

    change stylish and test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,8 @@ import { test, expect } from '@jest/globals';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import makeDiff from '../index.js';
+
+import genDiff from '../src/index.js';
 
 // const path = require('path');
 
@@ -10,13 +11,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 // console.log(getFixturePath('file1.json'));
-const expected = readFileSync(getFixturePath('expectedjson.txt'), 'utf-8');
+const expected = readFileSync(getFixturePath('expectedjson1.txt'), 'utf-8');
+// const expected1 = readFileSync(getFixturePath('expectedjson.txt'), 'utf-8');
 
-test('flat Json', () => {
-  expect(makeDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected);
+test('noflat Json', () => {
+  expect(genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'))).toBe(expected);
 });
-test('flat yml', () => {
-  expect(makeDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toBe(expected);
-  expect(makeDiff(getFixturePath('file3.yaml'), getFixturePath('file2.yml'))).toBe(expected);
-  expect(makeDiff(getFixturePath('file3.yaml'), getFixturePath('file2.yml'))).not.toBe('Unknown file format');
+test('noflat yml', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toBe(expected);
+  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file2.yml'))).toBe(expected);
+  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file2.yml'))).not.toBe('Unknown file format');
 });
--- startProgram.js
+++ startProgram.js
@@ -1,16 +1,16 @@
 import { program } from 'commander';
-import makeDiff from './index.js';
+import genDiff from './src/index.js';
 
 export default () => {
   program
     .version('0.0.1', '-V, --version', 'output the version number')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'choose format')
+    .option('-f, --format <type>', 'choose format', 'stylish')
     .argument('<filepath1>')
     .argument('<filepath2>')
     .action((filepath1, filepath2) => {
-      const result = makeDiff(filepath1, filepath2);
+      const result = genDiff(filepath1, filepath2, program.opts().format);
       console.log(result);
-    })
-    .parse(process.argv);
+    });
+  program.parse();
 };
